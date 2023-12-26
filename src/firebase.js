abc55caf3da2718a12b39c111fdc8260cc1c0689
sza619 <szahere619@gmail.com>
2023-12-26 23:05:19 +0530

    yarn, firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 // Initialize Firebase with your config
 const firebaseConfig = {
-  apiKey: "AIzaSyC0XB9RwEW1xm6rrtiWhDHL3yZOfgQn4CA",
-  authDomain: "taskmanager-24c39.firebaseapp.com",
-  projectId: "taskmanager-24c39",
-  storageBucket: "taskmanager-24c39.appspot.com",
-  messagingSenderId: "774732387843",
-  appId: "1:774732387843:web:ff64d7339dc9dc9b17a036",
+  apiKey: "AIzaSyDrnZ_jSyIUOEccayb-3ZBgnx_IdbQ-Srk",
+  authDomain: "taskify-f6e5a.firebaseapp.com",
+  projectId: "taskify-f6e5a",
+  storageBucket: "taskify-f6e5a.appspot.com",
+  messagingSenderId: "720671895849",
+  appId: "1:720671895849:web:9dae286b53e7a974c90738",
+  measurementId: "G-WTP3LBEBDH",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Cloud Messaging and get a reference to the service
 
+// Initialize Firebase auth and analytics
 const auth = getAuth(app);
+const analytics = getAnalytics(app);
 
-export { auth };
+export { auth, analytics };
